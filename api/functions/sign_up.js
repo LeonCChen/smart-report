752057a835c227217b5cc2a4c16fef1c4750b442
Dillon Morse <dillon@inoffensiveandwarm.com>
2021-03-16 21:00:53 -0400

    use hold Promise resolve on signup

--- api/functions/sign_up.js
+++ api/functions/sign_up.js
@@ -1,37 +1,39 @@
 var mysql = require('mysql');
 
-exports.handler = async function (event, context) {
-  var {email, hash, salt} = event.queryStringParameters;
-  email = decodeURIComponent(email);
-  hash = decodeURIComponent(hash);
-  salt = decodeURIComponent(salt);
+exports.handler = function (event) {
+  return new Promise(function (resolve, reject) {
+    var {email, hash, salt} = event.queryStringParameters;
+    email = decodeURIComponent(email);
+    hash = decodeURIComponent(hash);
+    salt = decodeURIComponent(salt);
 
-  var connection = mysql.createConnection({
-    host: '165.227.217.87',
-    user: 'test',
-    password: 'dillon',
-    database: 'the_smart_report'
+    var connection = mysql.createConnection({
+      host: '165.227.217.87',
+      user: 'test',
+      password: 'dillon',
+      database: 'the_smart_report'
+    });
+
+    connection.connect();
+
+    var sql_command = `INSERT INTO USER (email, hash, salt) VALUES ( '${email}', '${hash}', '${salt}')`
+    console.log(sql_command);
+    connection.query(sql_command, function (err, results) {
+      if (err) {
+        reject(err);
+      }
+      console.log('insert successful');
+      console.log(results);
+
+      resolve({
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        }
+      });
+    });
+
+    connection.end();
+
   });
-
-  connection.connect();
-
-  var sql_command = `INSERT INTO USER (email, hash, salt) VALUES ( '${email}', '${hash}', '${salt}')`
-  console.log(sql_command);
-  connection.query(sql_command, function (err, results) {
-    if (err)
-      throw err;
-    console.log('insert successful');
-    console.log(results);
-
-  });
-
-  connection.end();
-
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    }
-  };
-
 }
