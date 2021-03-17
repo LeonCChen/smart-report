276c0bbdfc617425d37bbd22e3ba9193d8a3ce7a
Dillon Morse <dillon@inoffensiveandwarm.com>
2021-03-16 21:34:54 -0400

    switch sql client to mariadb

--- api/functions/sign_up.js
+++ api/functions/sign_up.js
@@ -1,39 +1,32 @@
-var mysql = require('mysql');
+var mariadb = require('mariadb');
 
-exports.handler = function (event) {
-  return new Promise(function (resolve, reject) {
-    var {email, hash, salt} = event.queryStringParameters;
-    email = decodeURIComponent(email);
-    hash = decodeURIComponent(hash);
-    salt = decodeURIComponent(salt);
+exports.handler = async function (event) {
+  var {email, hash, salt} = event.queryStringParameters;
+  email = decodeURIComponent(email);
+  hash = decodeURIComponent(hash);
+  salt = decodeURIComponent(salt);
+  var sql_command = `INSERT INTO USER (email, hash, salt) VALUES ( '${email}', '${hash}', '${salt}')`
+  console.log(sql_command);
 
-    var connection = mysql.createConnection({
-      host: '165.227.217.87',
-      user: 'test',
-      password: 'dillon',
-      database: 'the_smart_report'
-    });
-
-    connection.connect();
-
-    var sql_command = `INSERT INTO USER (email, hash, salt) VALUES ( '${email}', '${hash}', '${salt}')`
-    console.log(sql_command);
-    connection.query(sql_command, function (err, results) {
-      if (err) {
-        reject(err);
-      }
+  await mariadb.createConnection({
+    host: '165.227.217.87',
+    user: 'test',
+    password: 'dillon',
+    database: 'the_smart_report'
+  }).then(connection => {
+    connection.query(sql_command).then(rows => {
       console.log('insert successful');
-      console.log(results);
-
-      connection.end();
-
-      resolve({
-        statusCode: 200,
-        headers: {
-          'Access-Control-Allow-Origin': '*'
-        }
-      });
+      console.log(rows);
+    }).catch(err => {
+      console.log(err);
     });
 
   });
-}
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  };
+};
+
