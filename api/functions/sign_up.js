69e95118468a44ec499e46a8169b817b1db44c42
Dillon Morse <dillon@inoffensiveandwarm.com>
2021-03-16 20:15:16 -0400

    setup CORS header on signup function

--- api/functions/sign_up.js
+++ api/functions/sign_up.js
@@ -2,25 +2,32 @@ var mysql = require('mysql');
 
 exports.handler = async function (event, context) {
 
-	var {email, hash, salt} = event.queryStringParamters;
+  var {email, hash, salt} = event.queryStringParamters;
 
-	var connection = mysql.createConnection ({
-		host:'165.227.217.87',
-		user: 'test',
-		password: 'dillon',
-		database: 'the_smart_report'
-	});
+  var connection = mysql.createConnection({
+    host: '165.227.217.87',
+    user: 'test',
+    password: 'dillon',
+    database: 'the_smart_report'
+  });
 
-	connection.connect();
+  connection.connect();
 
-	var sql_command = `INSERT INTO USER (email, hash, salt) VALUES ( '${email}', '${hash}', '${salt}')`
-	connection.query(sql_command, function(err, rows) {
-		if (err)
-			throw err;
-		console.dir(rows);
+  var sql_command = `INSERT INTO USER (email, hash, salt) VALUES ( '${email}', '${hash}', '${salt}')`
+  connection.query(sql_command, function (err, rows) {
+    if (err)
+      throw err;
+    console.dir(rows);
 
-	});
+  });
 
-	connection.end();
+  connection.end();
+
+  return {
+    statusCode: 200,
+    headers: {
+      'access-control-allow-origin': 'adoring-pare-ef65be.netlify.app'
+    }
+  };
 
 }
