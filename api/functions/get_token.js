ba7ab85efb6d8bd5ff479131d11872b1fa8ae432
Leon Chen <leonchen54@gmail.com>
2021-04-04 23:56:12 +0000

    Reworking get_token function

--- api/functions/get_token.js
+++ api/functions/get_token.js
@@ -1,27 +1,43 @@
 const mariadb = require('mariadb');
 const {randomBytes} = require('crypto');
 
-exports.handler = async function (event) {
+async function asyncFunction() {
 
-  var {email, hash} = event.queryStringParameters;
+/*
+	// Decoding the Variables
+	var {email, hash} = event.queryStringParameters;
   email = decodeURIComponent(email);
   hash = decodeURIComponent(hash);
+
+	// Uses the given email and has to get a TOKEN.user_id
   var sql_check_user = `SELECT USER.user_id FROM USER WHERE email='${email}' AND hash='${hash}';`
 
-  const conn = await mariadb.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
-  });
+
+	// Converts tex to a token
+  const buff = randomBytes(32);
+  const token = buff.toString('hex');
+
+	// Command For Inserting the TOKEN
+	var sql_insert_token = `INSERT into TOKEN (TOKEN.token, TOKEN.user_id) VALUES ( '${token}', ( SELECT USER.user_id from USER WHERE email='${email}' AND hash='${hash}' ) );`
+
+*/
+
+	console.log("Hello");
+	const conn = await mariadb.createConnection({
+		host: '165.227.217.87',
+		user: 'dillon',
+		password: 'wordword22',
+		database: 'the_smart_report'
+	});
 
   try {
-    const rows = await conn.query(sql_check_user);
+		
+    const rows = await conn.query('SELECT * FROM USER;');
     // Checks if the User is in the Database
-    console.log('CHECKS if USER IS IN DB');
+    console.log('SQL COMMAND 1 WORKED!!!');
     console.log(rows[0])
 
-    // If does not exist
+    // If the Query returns nothing!
     if (!(rows[0])) {
       return {
         statusCode: 403,
@@ -30,8 +46,17 @@ exports.handler = async function (event) {
         }
       };
     }
-  } catch {
-    // Error
+
+
+		// Query to inset the 
+		const mytoken = await conn.query('SELECT * FROM USER;');
+		
+		console.log('SQL COMMAND 2 WORKED!!!');
+		console.log(mytoken[1]);
+
+  } 
+	catch {
+    console.log('IT DOES NOT WORK!!!');
     return {
       statusCode: 401,
       headers: {
@@ -39,36 +64,16 @@ exports.handler = async function (event) {
       }
     };
   }
-  // Converts token in hex
-  const buff = randomBytes(32);
-  const token = buff.toString('hex');
-
-  // SQL COMMAND TO INSERT
-  var sql_insert_token = `INSERT into TOKEN (TOKEN.token, TOKEN.user_id) VALUES ( '${token}', ( SELECT USER.user_id from USER WHERE email='${email}' AND hash='${hash}' ) );`
-
-  try {
-    await conn.query(sql_get_token)
-    // Inserts the New token in to the database
-    console.log(rows[0]);
-    return ("INSERT SUCCESSFUL")
-
-  } catch {
-    // Errors
-    console.log("OOPS SOMETHING WENT WRONG");
-
-    return {
-      statusCode: 401,
-      header: {
-        'Access-Control-Allow-Origin': '*'
-      }
-    };
-  }
-
-  // Success
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    }
-  };
+	
+	console.log('Right before return');
+	// Success
+	return {
+		statusCode: 200,
+		headers: {
+			'Access-Control-Allow-Origin': '*'
+		}
+	};	
 };
+
+
+asyncFunction();
